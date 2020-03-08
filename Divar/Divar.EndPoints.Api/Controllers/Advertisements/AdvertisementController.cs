c20eac159e59bb2a7db9d0d99d00b2cfb521c2ad
shahabnoori <noorigoodarzi@gmail.com>
2020-03-08 14:16:20 +0330

    Remove Divar.EndPoints.Api Project

---
+++ Divar/Divar.EndPoints.Api/Controllers/Advertisements/AdvertisementController.cs
@@ -0,0 +1,47 @@
+﻿//using Microsoft.AspNetCore.Mvc;
+//namespace Divar.EndPoints.Api.Controllers.Advertisements
+//{
+//    [ApiController]
+//    [Route("[controller]")]
+//    public class AdvertisementController : ControllerBase
+//    {
+//        [HttpPost]
+//        public IActionResult Post([FromServices] CreateHandler handler, CreateCommand request)
+//        {
+//            handler.Handle(request);
+//            return Ok();
+//        }
+
+//        [Route("title")]
+//        [HttpPut]
+//        public IActionResult Put([FromServices] SetTitleHandler handler, SetTitle request)
+//        {
+//            handler.Handle(request);
+//            return Ok();
+//        }
+
+//        [Route("text")]
+//        [HttpPut]
+//        public IActionResult Put([FromServices] UpdateTextHandler handler, UpdateText request)
+//        {
+//            handler.Handle(request);
+//            return Ok();
+//        }
+
+//        [Route("price")]
+//        [HttpPut]
+//        public IActionResult Put([FromServices] UpdatePriceHandler handler, UpdatePrice request)
+//        {
+//            handler.Handle(request);
+//            return Ok();
+//        }
+
+//        [Route("publish")]
+//        [HttpPut]
+//        public IActionResult Put([FromServices] RequestToPublishHandler handler, RequestToPublish request)
+//        {
+//            handler.Handle(request);
+//            return Ok();
+//        }
+//    }
+//}
