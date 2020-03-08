7dc328d28db84bda51b5a495fceedd5dc83bfaa9
shahabnoori <noorigoodarzi@gmail.com>
2020-03-08 14:03:14 +0330

    Initial Advertisement Fake Repository

---
+++ Divar/Divar.Infrastructures.Data.Fake/Advertisements/FakeAdvertisementRepository.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using Divar.Core.Domain.Advertisements.Data;
+using Divar.Core.Domain.Advertisements.Entities;
+
+namespace Divar.Infrastructures.Data.Fake.Advertisements
+{
+    public class FakeAdvertisementRepository : IAdvertisementRepository
+    {
+        private readonly Dictionary<Guid, Advertisement> _db = new Dictionary<Guid, Advertisement>();
+
+        public bool Exists(Guid id)
+        {
+            return _db.ContainsKey(id);
+        }
+
+        public Advertisement Load(Guid id)
+        {
+            return _db[id];
+        }
+
+        public void Save(Advertisement entity)
+        {
+            _db[entity.Id] = entity;
+        }
+    }
+}
